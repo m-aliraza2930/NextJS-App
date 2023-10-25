200cdb55cbae7b73b2a796bca7afcbf288edc57f
m-aliraza2930 <alishah40440@gmail.com>
2023-10-25 10:25:35 +0500

    first commit
    first commit

---
+++ src/sections/home/home-hero.tsx
@@ -0,0 +1,36 @@
+import type { FC } from 'react';
+import EyeIcon from '@untitled-ui/icons-react/build/esm/Eye';
+import LayoutBottomIcon from '@untitled-ui/icons-react/build/esm/LayoutBottom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import SvgIcon from '@mui/material/SvgIcon';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+
+import { RouterLink } from 'src/components/router-link';
+import { paths } from 'src/paths';
+
+import { HomeCodeSamples } from './home-code-samples';
+import Page from 'src/app/auth/jwt/login/page';
+
+export const HomeHero: FC = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top center',
+        backgroundImage: 'url("/assets/gradient-bg.svg")',
+        pt: '120px',
+      }}
+    >
+      <Container maxWidth="sm">
+        <Page />
+      </Container>
+    </Box>
+  );
+};
