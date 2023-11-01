e39e33018ab2dd3ed5a4417a5b550cf36eaf464d
Muhmmad Shahzaib <58455625+shahzaib803@users.noreply.github.com>
2023-11-01 18:18:09 +0500

    comment the drawer extra links

--- src/layouts/dashboard/vertical-layout/side-nav.tsx
+++ src/layouts/dashboard/vertical-layout/side-nav.tsx
@@ -231,7 +231,7 @@ export const SideNav: FC<SideNavProps> = (props) => {
               />
             ))}
           </Stack>
-          <Box sx={{ p: 3 }}>
+          {/* <Box sx={{ p: 3 }}>
             <Typography variant="subtitle1">Need help?</Typography>
             <Typography
               color="neutral.400"
@@ -254,7 +254,7 @@ export const SideNav: FC<SideNavProps> = (props) => {
             >
               Documentation
             </Button>
-          </Box>
+          </Box> */}
         </Stack>
       </Scrollbar>
     </Drawer>
